3cf7d73b732a45fb0fe16297432b900812ebba01
Baptiste Machard <21904691@etu.unicaen.fr>
2021-03-18 13:02:31 +0100

    maquette

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,13 +1,16 @@
 <?php
 namespace controllers;
  use Ubiquity\controllers\admin\popo\Route;
+ use Ubiquity\core\postinstall\Display;
 
  /**
   * Controller MainController
   */
 class MainController extends ControllerBase{
-    #[Route(path : "projet")]
+
+
+    #[Route(path : "_default")]
 	public function index(){
-		
+
 	}
 }
