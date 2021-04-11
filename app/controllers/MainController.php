39639f21fbf7421555c0fe22a9c67fb5829363f0
Baptiste Machard <21904691@etu.unicaen.fr>
2021-04-11 23:50:15 +0200

    oublie

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,13 +1,43 @@
 <?php
 namespace controllers;
+ use models\Basket;
+ use models\Order;
+ use models\Product;
+ use models\Section;
  use Ubiquity\controllers\admin\popo\Route;
+ use Ubiquity\controllers\auth\AuthController;
+ use Ubiquity\controllers\auth\WithAuthTrait;
+ use Ubiquity\core\postinstall\Display;
+ use Ubiquity\orm\DAO;
 
  /**
   * Controller MainController
   */
 class MainController extends ControllerBase{
-    #[Route(path : "projet")]
+    use WithAuthTrait;
+
+    #[Route(path : "_default")]
 	public function index(){
-		
+        $user=$this->getAuthController()->_getActiveUser();
+        $nbcommande=DAO::count(Order::class,"iduser=?",[$user->getId()]);
+        $nbPanier =DAO::count(Basket::class,"iduser=?",[$user->getId()]);
+        $sections=DAO::getAll(Section::class);
+        $prodReds=DAO::getAll(Product::class,"promotion!=0.00");
+        $this->loadDefaultView(compact("user","nbcommande","sections","nbPanier","prodReds"));
 	}
+
+    protected function getAuthController(): AuthController
+    {
+        return new Login($this);
+    }
+    #[Route(path: "section/{idSection}")]
+    public function detailSection($idSection){
+        $this->loadView();
+    }
+   /* public function getPrixPanier($user){
+        $list=DAO::getAll(Product::class,"iduser=? and ",[$user->getId()]);
+        foreach ($p : $list){
+
+        }
+    }*/
 }
